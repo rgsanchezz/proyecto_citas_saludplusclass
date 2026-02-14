439e22809d1e32ff4142b6b7cc395af8710d67c8
ticsat <rg.sanchezz@uea.edu.ec>
2026-02-14 10:01:10 -0500

    segunda version proyecto clase

--- app.py
+++ app.py
@@ -17,5 +17,10 @@ def cita(paciente):
 def especialidad(nombre):
     return f"Especialidad consultada: {nombre}. Disponibilidad sujeta a agenda médica."
 
-if __name__ == '__main__':
-    app.run(debug=True)
+#if __name__ == '__main__':
+ #   app.run(debug=True)
+import os
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
